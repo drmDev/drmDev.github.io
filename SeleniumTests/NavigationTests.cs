c575d466c1395e479171095cd10c9fdc9bd7a8c5
Derek Monturo <dmontur1@gmail.com>
2024-12-23 20:08:22 -0500

    more detailed hobbies. updated selenium tests

--- SeleniumTests/NavigationTests.cs
+++ SeleniumTests/NavigationTests.cs
@@ -7,7 +7,7 @@ namespace SeleniumTests
     public class NavigationTests : BaseTest
     {
         [Test]
-        public void HobbiesPage_ShouldRender_AndShowFilters()
+        public void HobbiesPage_ShouldRender()
         {
             // Navigate to the landing page
             driver.Navigate().GoToUrl("https://drmdev.github.io/");
@@ -15,6 +15,13 @@ namespace SeleniumTests
             // Click on the "Hobbies" navbar link
             var hobbiesLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(), 'Hobbies')]")));
             hobbiesLink.Click();
+
+            // Verify specific elements are rendered on the Hobbies page
+            var chessIcon = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//i[contains(@class, 'fa-chess-king')]")));
+            chessIcon.Should().NotBeNull("Chess icons should be present on the Hobbies page.");
+
+            var mtgIcon = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//img[@alt='Plains']")));
+            mtgIcon.Should().NotBeNull("MTG icons should be present on the Hobbies page.");
         }
     }
 }
