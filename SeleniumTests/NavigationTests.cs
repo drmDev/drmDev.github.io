755e08612d6a62d04f5197e41c70132e4300814f
Derek Monturo <dmontur1@gmail.com>
2025-02-06 15:21:10 -0500

    fix selenium test

--- SeleniumTests/NavigationTests.cs
+++ SeleniumTests/NavigationTests.cs
@@ -9,12 +9,14 @@ namespace SeleniumTests
         [Test]
         public void HobbiesPage_ShouldRender()
         {
+            // Navigate to the landing page
             driver.Navigate().GoToUrl("https://drmdev.github.io/");
 
             // Click on the "Hobbies" navbar link
-            var hobbiesLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(), 'Hobbies')]")));
+            var hobbiesLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("nav-hobbies")));
             hobbiesLink.Click();
 
+            // Verify the filter section is visible
             var filterSection = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("filter-board-games")));
             filterSection.Displayed.Should().BeTrue("The filter section should be visible on the Hobbies page.");
         }
--- SeleniumTests/SmokeTests.cs
+++ SeleniumTests/SmokeTests.cs
@@ -1,19 +1,22 @@
-using FluentAssertions;
+using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 using SeleniumExtras.WaitHelpers;
+using SeleniumTests;
 
-namespace SeleniumTests
+public class NavigationTests : BaseTest
 {
-    public class SmokeTests : BaseTest
+    [Test]
+    public void HobbiesPage_ShouldRender()
     {
-        [Test]
-        public void LandingPage_ShouldRender_AndShowResumeLink()
-        {
-            driver.Navigate().GoToUrl("https://drmdev.github.io/");
+        // Navigate to the landing page
+        driver.Navigate().GoToUrl("https://drmdev.github.io/");
 
-            var resumeLink = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[contains(@href, '/assets/pdf/DM_Resume_2024.pdf')]")));
+        // Click on the "Hobbies" navbar link
+        var hobbiesLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("nav-hobbies")));
+        hobbiesLink.Click();
 
-            resumeLink.Displayed.Should().BeTrue("Resume link is not visible on the landing page.");
-        }
+        // Verify the filter section is visible
+        var filterSection = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("filter-board-games")));
+        filterSection.Displayed.Should().BeTrue("The filter section should be visible on the Hobbies page.");
     }
 }
