94f39a493ee91eecf0ea32fddfc0a08ee27ef2d4
Derek Monturo <dmontur1@gmail.com>
2025-07-15 05:03:06 -0400

    remove obsolete refs

--- SeleniumTests/Pages/HomePage.cs
+++ SeleniumTests/Pages/HomePage.cs
@@ -5,9 +5,6 @@ namespace SeleniumTests.Pages
 {
     public class HomePage : BasePage
     {
-        private readonly By ResumeLink = By.Id("resume-download-link");
-        private readonly By ResumeViewer = By.Id("resume-viewer");
-
         public HomePage(IWebDriver driver) : base(driver) { }
 
         // fluent method for chaining
@@ -15,18 +12,6 @@ namespace SeleniumTests.Pages
         {
             Driver.Navigate().GoToUrl(baseUrl);
             return this;
-        }
-
-        public bool IsResumeLinkDisplayed()
-        {
-            var resumeLink = Wait.Until(ExpectedConditions.ElementIsVisible(ResumeLink));
-            return resumeLink.Displayed;
-        }
-
-        public bool IsResumeViewerDisplayed()
-        {
-            var resumeViewer = Wait.Until(ExpectedConditions.ElementIsVisible(ResumeViewer));
-            return resumeViewer.Displayed;
         }
 
         public HobbiesPage NavigateToHobbies()
